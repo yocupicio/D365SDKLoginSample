3d5112b52c84b9434305ace765eff01adf1c43a6
yocupicio <63861813+yocupicio@users.noreply.github.com>
2020-05-12 18:48:42 -0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D365SDKLoginSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var frm = new ExampleLoginForm();
+            frm.ShowDialog();
+            // If the login process completed, assign the connected service to the CRMServiceClient var 
+            if (frm.CrmConnectionMgr != null && frm.CrmConnectionMgr.CrmSvc != null && frm.CrmConnectionMgr.CrmSvc.IsReady)
+            {
+                //service = frm.CrmConnectionMgr.CrmSvc;
+            }
+            // If you are not connected to CRM; display the last error and last CRM excption
+            else
+            {
+                MessageBox.Show("The connection to CRM failed or was cancelled by the user.");
+            }
+        }
+    }
+}
